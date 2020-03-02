e9c30f61e3318aba1db04f3b23238351ae7a576a
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-02 14:31:19 +0000

    Add navbar content and styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledNav = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: gray;
+    margin: 0 auto;
+    padding: 15px 40px;
+    color: white;
+
+    #logo {
+        font-weight: bold;
+        font-size: 2rem;
+    }
+
+    #links {
+        ul {
+            display: flex;
+            justify-content: flex-end;
+            li {
+                font-size: 1.2rem;
+                list-style-type: none;
+                margin-left: 20px;
+            }
+        }
+    }
+`
 
 function NavBar() {
     return (
-        <div>
-            This will contain the logo and About / Login links
-        </div>
+        <StyledNav>
+            <div id='logo'>Treasure Quest</div>
+            <div id='links'>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+        </StyledNav>
     );
 }
 
