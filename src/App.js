44280ffb6267116c04d747eb33d507ec47e3743e
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-02 15:31:05 +0000

    Add styling to homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import styled from 'styled-components';
 
 import NavBar from "./components/NavBar";
 import RoomDetail from "./components/RoomDetail";
@@ -7,19 +7,45 @@ import Navigation from "./components/Navigation";
 import Map from "./components/Map";
 import Chat from "./components/Chat";
 
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  #game-container {
+    display: flex;
+    margin: 0 auto;
+
+    #left {
+      width: 60vw;
+      height: 60vw;
+      border: 1px red solid;
+    }
+
+    #right {
+      width: 20vw;
+      height: 60vw;
+      border: 1px red solid;
+      display: flex;
+      flex-direction: column;
+    }
+  }
+`
+
 function App() {
   return (
-    <div className="App">
+    <StyledApp>
       <NavBar />
-      <div id="left">
-        <Map />
+      <div id='game-container'>
+        <div id="left">
+          <Map />
+        </div>
+        <div id="right">
+          <RoomDetail />
+          <Navigation />
+          <Chat />
+        </div>
       </div>
-      <div id="right">
-        <RoomDetail />
-        <Navigation />
-        <Chat />
-      </div>
-    </div>
+    </StyledApp>
   );
 }
 
