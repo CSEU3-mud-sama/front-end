1375ebda057c331d3569f2320bd8944d58484a9f
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-03 10:42:26 +0000

    Add buttons to Controller

--- src/components/Controller.jsx
+++ src/components/Controller.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+function NavButton(label, callback) {
+    return (
+        <button onClick={callback}>{label}</button>
+    )
+}
+
 function Navigation() {
     return (
-        <div>This will show the current navigation options (go north, south, east and west)</div>
+        <div>
+            {['N', 'S', 'E', 'W'].map(label => NavButton(label))}
+        </div>
     );
 }
 
