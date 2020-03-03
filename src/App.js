a9ff34c981fc4d1ab697aa1fbb69e890e32c41ce
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-03 08:25:50 +0000

    Add reset.css

--- src/App.js
+++ src/App.js
@@ -1,56 +1,8 @@
 import React from "react";
-import styled from 'styled-components';
-
-import NavBar from "./components/NavBar";
-import RoomDetail from "./components/RoomDetail";
-import Navigation from "./components/Controller";
-import Map from "./components/Map";
-import Chat from "./components/Chat";
-
-const StyledApp = styled.div`
-  display: flex;
-  flex-direction: column;
-  box-sizing: border-box;
-
-  #game-container {
-    display: flex;
-    margin: 0 auto;
-
-    #left {
-      width: 60vw;
-      height: 60vw;
-      border: 1px red solid;
-      margin: 3px;
-      padding: 5px;
-    }
-
-    #right {
-      width: 25vw;
-      height: 60vw;
-      border: 1px red solid;
-      display: flex;
-      flex-direction: column;
-      margin: 3px;
-      padding: 5px;
-    }
-  }
-`
 
 function App() {
   return (
-    <StyledApp>
-      <NavBar />
-      <div id='game-container'>
-        <div id="left">
-          <Map />
-        </div>
-        <div id="right">
-          <RoomDetail />
-          <Navigation />
-          <Chat />
-        </div>
-      </div>
-    </StyledApp>
+    <div>This will show the app</div>
   );
 }
 
