1f3a687efb5c21a9b1cc5bf7b8fb9cfa4c580f8c
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-03-05 10:49:16 +0000

    Map grid

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,60 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import LeftPanel from '../styles/LeftPanel'
+
+const StyledMap = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 2rem 6rem;
+
+    .row {
+        display: flex;
+        height: 11rem;
+    }
+
+    .cell {
+        flex-grow: 1;
+    }
+
+    .top {
+        border-bottom: 1px solid blue;
+    }
+
+    .bottom {
+        border-top: 1px solid blue;
+    }
+
+    .left {
+        border-right: 1px solid blue;
+    }
+
+    .right {
+        border-left: 1px solid blue;
+    }
+`
 
 function Map(props) {
     return (
         <LeftPanel>
             <h2>Map</h2>
+            <StyledMap>
+                <div className='row'>
+                    <div className='cell top left'></div>
+                    <div className='cell center'></div>
+                    <div className='cell top right'></div>
+                </div>
+                <div className='row'>
+                    <div className='cell mid'></div>
+                    <div className='cell mid center'></div>
+                    <div className='cell mid'></div>
+                </div>
+                <div className='row'>
+                    <div className='cell bottom left'></div>
+                    <div className='cell center'></div>
+                    <div className='cell bottom right'></div>
+                </div>
+            </StyledMap>
         </LeftPanel>
     );
 }
